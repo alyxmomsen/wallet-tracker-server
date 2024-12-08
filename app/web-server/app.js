ea586ed040bc4a37a0ea570d1d6a800f3fda2d7c
alex <shizofranky@gmail.com>
2024-12-08 15:30:14 +0300

    'update'

--- app/web-server/app.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.WebServerDriver = void 0
-class WebServerDriver {
-    constructor() {}
-}
-exports.WebServerDriver = WebServerDriver
